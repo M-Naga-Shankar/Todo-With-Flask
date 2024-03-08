04b7dfc505150e73f8f142ef3ff7d8dee8fdebb3
nagashankar-01 <logger3845@gmail.com>
2024-03-08 23:47:27 +0530

    database added

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+app.app_context().push()
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    desc = db.Column(db.String)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
 
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
 
 
 @app.route('/')
